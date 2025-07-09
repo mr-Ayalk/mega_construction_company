3dd8c9870975633b255aa9ff43dc017d0ec9c167
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-09 23:42:38 +0300

    contact page completed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
           initial="hidden"
           whileInView="visible"
           variants={slideUpVariants}
-          className="lg:w-[60%] w-full flex flex-col justify-between items-startgap-6"
+          className="lg:w-[60%] w-full flex flex-col justify-between items-start gap-6"
         >
           <motion.h1
             variants={slideUpVariants}
@@ -28,42 +28,45 @@ const Contact = () => {
           </motion.h1>
 
           <div className="w-[120px] h-[6px] bg-yellow-500 "></div>
-          <p className="text-3xl italic text-gray-50 mt-[60px]">
+          <p className="text-3xl italic text-gray-600 mt-[60px]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-            voluptatem sint ab dolorem esse qui amet, doloribus in cupiditate
-            veritatis! Soluta animi nesciunt ea atque. Perspiciatis molestiae
-            deserunt aperiam quas?
+            voluptatem sint ab dolorem esse
           </p>
         </motion.div>
         <motion.div
           initial="hidden"
           whileInView="visible"
-          values={slideUpVariants}
+          variants={slideUpVariants}
           className="lg:w-[40%] w-full flex flex-col justify-center items-start gap-6"
         >
           <motion.form
-            initial="hidden"
-            whileHover="visible"
             variants={zoomInVariants}
             className="flex flex-col justify-center items-start gap-4 w-full"
           >
             <input
-              type="text"
-              placeholder="Enter Fullname"
-              className="px-6 py-3 border-2px] border-black text-black rounded-lg w-full"
-            />
-
-            <input
               type="email"
-              placeholder="EnterEmail"
-              className="px-6 py-3 border-2px] border-black text-black rounded-lg w-full"
+              placeholder="Enter Email"
+              className="px-6 py-3 border-2 border-black text-black rounded-lg w-full"
             />
 
             <input
               type="number"
               placeholder="Enter Mobile Number"
-              className="px-6 py-3 border-2px] border-black text-black rounded-lg w-full"
+              className="px-6 py-3 border-2 border-black text-black rounded-lg w-full"
             />
+
+            <textarea
+              placeholder="Enter your message"
+              rows="4"
+              className="px-6 py-3 border-2 border-black text-black rounded-lg w-full"
+            />
+
+            <motion.button
+              variants={zoomInVariants}
+              className="bg-yellow-500 hover:bg-black hover:text-white px-10 py-4 text-black font-bold rounded-lg w-full"
+            >
+              SUBMIT
+            </motion.button>
           </motion.form>
         </motion.div>
       </div>
