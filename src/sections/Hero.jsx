3fcf9e5d6f7294baae73a7983dc409288b01b6c7
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-10 00:38:45 +0300

    project completed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { slideUpVariants, zoomInVariants } from "./animation";
 const Hero = () => {
   return (
+    // must be fixed the height for mobile view
     <div
       id="home"
       className="bg-black w-full lg:h-lvh h-fit m-auto pt-[60px] lg:pt-[0px] lg:px-[150px] px-[20px] flex justify-between items-center lg:flex-row flex-col lg:gap-5 gap-[50px] bg-cover bg-center "
