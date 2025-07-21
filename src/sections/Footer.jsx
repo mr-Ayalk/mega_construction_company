4960c8544dc26d4d667a5fd6b0d8636084e554b8
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-22 00:00:35 +0300

    footer page section updated

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,21 +5,23 @@ import { FaCopyright } from "react-icons/fa6";
 const Footer = () => {
   return (
     <>
-      <div className="bg-black text-white flex justify-center items-center gap-2 p-5 ">
-        <FaCopyright className="fill-yellow-500 lg:size-5 size-8" />
+      <div className="bg-gray-700 text-white flex justify-center items-center gap-2 p-5 ">
+        <span className="!fill-yellow-500 lg:size-5 size-8"> &copy;</span>
+
+        {/* <FaCopyright  /> */}
         <p className="text-lg text-center ">
-          Copyright 2025 ,MEGA CONSTRUCTION,All Rights Reserved
+          Copyright 2025 ,TOP CONSTRUCTION,All Rights Reserved
         </p>
       </div>
       {/* scroll to top button */}
-      <div
+      {/* <div
         id="icon-box"
         className="bg-yellow-500 text-black p-3 rounded-full hover:bg-black hover:text-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6"
       >
         <Link to="home" spy={true} offset={-100} smooth={true}>
           <FaArrowUp className="size-6" />
         </Link>
-      </div>
+      </div> */}
     </>
   );
 };
